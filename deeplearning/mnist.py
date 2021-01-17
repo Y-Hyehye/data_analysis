1934f37e72ad74d691a2933a3c29d5db4eda3d3d
yanghyeji <yang_hyeji1@daum.net>
2021-01-17 23:04:09 +0900

    deeplearning

---
+++ deeplearning/mnist.py
@@ -0,0 +1,114 @@
+import tensorflow as tf
+import random
+import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# hyper parameters
+learning_rate = 0.001
+training_epochs = 20
+batch_size = 100
+
+# dropout (keep_prob) rate  0.7~0.5 on training, but should be 1 for testing
+keep_prob = tf.placeholder(tf.float32)
+
+# input place holders
+X = tf.placeholder(tf.float32, [None, 784])
+X_img = tf.reshape(X, [-1, 28, 28, 1])   # img 28x28x1 (black/white)
+Y = tf.placeholder(tf.float32, [None, 10])
+
+# L1
+W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
+L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME')
+L1 = tf.nn.relu(L1)
+L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
+
+# L2
+W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
+L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
+L2 = tf.nn.relu(L2)
+L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
+
+# L3
+W3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
+L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
+L3 = tf.nn.relu(L3)
+L3 = tf.nn.max_pool(L3, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
+L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
+
+# L4
+W4 = tf.Variable(tf.random_normal([3, 3, 128, 256], stddev=0.01))
+L4 = tf.nn.conv2d(L3, W4, strides=[1, 1, 1, 1], padding='SAME')
+L4 = tf.nn.relu(L4)
+L4 = tf.nn.max_pool(L4, ksize=[1, 2, 2, 1], strides=[
+                    1, 2, 2, 1], padding='SAME')
+L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
+L4_flat = tf.reshape(L4, [-1, 256 * 2 * 2])
+
+# L5 FC
+W5 = tf.get_variable("W5", shape=[256 * 2 * 2, 256],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([256]))
+L5 = tf.nn.relu(tf.matmul(L4_flat, W5) + b5)
+L5 = tf.nn.dropout(L5, keep_prob=keep_prob)
+
+# L6 FC
+W6 = tf.get_variable("W6", shape=[256, 512],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b6 = tf.Variable(tf.random_normal([512]))
+L6 = tf.nn.relu(tf.matmul(L5, W6) + b6)
+L6 = tf.nn.dropout(L6, keep_prob=keep_prob)
+
+# L7 Final FC
+W7 = tf.get_variable("W7", shape=[512, 10],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b7 = tf.Variable(tf.random_normal([10]))
+logits = tf.matmul(L6, W7) + b7
+
+# define cost/loss & optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# initialize
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+# train my model
+print('Learning started. It takes sometime.')
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feed_dict = {X: batch_xs, Y: batch_ys, keep_prob: 0.7}
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c / total_batch
+
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+
+print('Learning Finished!')
+
+# Test model and check accuracy
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy:', sess.run(accuracy, feed_dict={
+      X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1}))
+
+# Get one and predict
+r = random.randint(0, mnist.test.num_examples - 1)
+print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
+print("Prediction: ", sess.run(
+    tf.argmax(logits, 1), feed_dict={X: mnist.test.images[r:r + 1], keep_prob: 1}))
+
+plt.imshow(mnist.test.images[r:r + 1].
+           reshape(28, 28), cmap='Greys', interpolation='nearest')
+plt.show()
